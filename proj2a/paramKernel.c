92670dfef20b148fd43d78ead57df13d7c1990a3
Gu Yao <gamesgao@gmail.com>
2017-03-28 03:42:03 -0700

    添加支持array string 的参数功能

--- proj2a/paramKernel.c
+++ proj2a/paramKernel.c
@@ -3,12 +3,24 @@
 #include <linux/init.h>
 #include <linux/moduleparam.h>
 
-static int kernel_number;
-module_param(kernel_number, int, 0644);
+static int param_int = 0;
+static char* param_str = "no parameter input.\n            ";
+static int param_array[200] = {0};
+static int param_array_num = 0;
+module_param(param_int, int, 0644);
+module_param(param_str, charp, 0644);
+module_param_array(param_array, int, &param_array_num, 0644);
 
 static int __init hello_init(void){
+    int i=0;
     printk(KERN_INFO "Hello world\n");
-    printk(KERN_INFO "Param:kernel_number:%d;\n", kernel_number);
+    printk(KERN_INFO "Param:param_int:%d;\n", param_int);
+    printk(KERN_INFO "Param:param_str:%s;\n", param_str);
+    printk(KERN_INFO "Param:param_array:");
+    for(i =0;i<param_array_num;i++){
+        printk("%d ", param_array[i]);
+    }
+    printk(";\n");
     return 0;
 }
 
